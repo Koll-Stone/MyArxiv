e9d63e9c6d0fb662eda0086d7c38e324b0342dbb
Koll-Stone <qiweihuu@gmail.com>
2024-08-20 14:58:21 +0800

    update asia time

--- statics/index.js
+++ statics/index.js
@@ -33,7 +33,14 @@ if (currentTheme) {
 }
 
 const timestamp = document.getElementById("build-timestamp");
-const timestamp_local = new Date(timestamp.getAttribute("datetime")).toLocaleString();
+const timestamp_utc = new Date(timestamp.getAttribute("datetime"));
+
+const timestamp_local = timestamp_utc.toLocaleString('zh-CN', {
+    timeZone: 'Asia/Shanghai',
+    hour12: false,
+});
+
+// const timestamp_local = new Date(timestamp.getAttribute("datetime")).toLocaleString();
 
 const badge = document.getElementById("build-timestamp-badge");
 // badge.src = `https://img.shields.io/github/workflow/status/mlnlp-world/myarxiv/Update?=${timestamp_local}&style=for-the-badge`
